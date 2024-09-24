90dfad19c9ec929d2ad132f9a0885c2608ee976e
umitt <umit.salman@glomil.com>
2024-09-24 05:49:39 +0300

    feat: create dashboard layout structure

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -3,5 +3,14 @@ export default function DashboardLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  return <div>Dashboard Layout{children}</div>
+  return (
+    <div className="h-screen flex">
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
+        left menu
+      </div>
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">
+        {children}
+      </div>
+    </div>
+  )
 }
