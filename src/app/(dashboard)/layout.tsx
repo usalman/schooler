5ccee8b5e324127705f4cdb5d09a30a746c0f9fe
umitt <umit.salman@glomil.com>
2024-09-24 06:29:20 +0300

    feat: complete left menu

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -9,7 +9,7 @@ export default function DashboardLayout({
 }>) {
   return (
     <div className="h-screen flex">
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200 p-4">
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
         <Link
           href="/"
           className="flex items-center justify-center lg:justify-start gap-2"
@@ -19,9 +19,7 @@ export default function DashboardLayout({
         </Link>
         <Menu />
       </div>
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">
-        {children}
-      </div>
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]">{children}</div>
     </div>
   )
 }
--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -1,11 +1,25 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import menuItems from '@/shared/menuItems'
 
 const Menu = () => {
   return (
-    <div>
+    <div className="mt-4 text-sm">
       {menuItems.map((menuItem) => (
-        <div className="" key={menuItem.title}>
-          <span>{menuItem.title}</span>
+        <div className="flex flex-col gap-2" key={menuItem.title}>
+          <span className="hidden lg:block text-gray-400 font-light my-4">
+            {menuItem.title}
+          </span>
+          {menuItem.items.map((item) => (
+            <Link
+              href={item.href}
+              key={item.label}
+              className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2"
+            >
+              <Image src={item.icon} alt="" width={20} height={20} />
+              <span className="hidden lg:block">{item.label}</span>
+            </Link>
+          ))}
         </div>
       ))}
     </div>
