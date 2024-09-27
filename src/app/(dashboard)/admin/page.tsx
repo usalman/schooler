6e6704c49e9277400231a60a52e9e5c78479a64b
umitt <umit.salman@glomil.com>
2024-09-27 19:40:24 +0300

    feat: create UserCard for admin page

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,19 @@
+import UserCard from '@/components/UserCard'
+
 const AdminPage = () => {
-  return <div className="">Admin Page</div>
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      <div className="w-full lg:w-2/3">
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+      </div>
+      <div className="w-full lg:w-1/3">right</div>
+    </div>
+  )
 }
 
 export default AdminPage
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,25 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        Sky: '#C3EBFA',
+        SkyLight: '#EDF9FD',
+        Purple: '#CFCEFF',
+        PurpleLight: '#F1F0FF',
+        Yellow: '#FAE27C',
+        YellowLight: '#FEFCE8',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
