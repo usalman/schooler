be0859b40cb28a7f84841101335c41a610ead112
umitt <umit.salman@glomil.com>
2024-10-02 01:17:21 +0300

    feat: create student page skeleton

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,5 +1,21 @@
+import Announcements from '@/components/Announcements'
+import EventCalendar from '@/components/EventCalendar'
+
 const StudentPage = () => {
-  return <div className="">Student Page</div>
+  return (
+    <div className="p-4 flex gap-4 flex-col xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3"></div>
+      <div className="h-full bg-white p-4 rounded-md">
+        <h1 className="text-xl font-semibold">Schedule(4A)</h1>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements />
+      </div>
+    </div>
+  )
 }
 
 export default StudentPage
