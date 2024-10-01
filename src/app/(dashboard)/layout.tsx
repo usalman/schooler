b7ba1dde64e5ec8a601ce9643a3d04f9dd495e4d
umitt <umit.salman@glomil.com>
2024-10-02 02:22:29 +0300

    feat: complete parent, student and teacher pages

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -20,7 +20,7 @@ export default function DashboardLayout({
         </Link>
         <Menu />
       </div>
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#f7f8fa] overflow-scroll">
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#f7f8fa] overflow-scroll flex flex-col">
         <Navbar />
         {children}
       </div>
