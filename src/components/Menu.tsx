a1a8cb6ed9d6785416e9a782c2d5c8f2848a9f9e
umitt <umit.salman@glomil.com>
2024-10-02 01:08:39 +0300

    dev: use user role info from external data to show menu items

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import menuItems from '@/shared/menuItems'
+import { role } from '@/shared/data'
 
 const Menu = () => {
   return (
@@ -10,16 +11,20 @@ const Menu = () => {
           <span className="hidden lg:block text-gray-400 font-light my-4">
             {menuItem.title}
           </span>
-          {menuItem.items.map((item) => (
-            <Link
-              href={item.href}
-              key={item.label}
-              className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2"
-            >
-              <Image src={item.icon} alt="" width={20} height={20} />
-              <span className="hidden lg:block">{item.label}</span>
-            </Link>
-          ))}
+          {menuItem.items.map((item) => {
+            if (item.visible.includes(role)) {
+              return (
+                <Link
+                  href={item.href}
+                  key={item.label}
+                  className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2"
+                >
+                  <Image src={item.icon} alt="" width={20} height={20} />
+                  <span className="hidden lg:block">{item.label}</span>
+                </Link>
+              )
+            }
+          })}
         </div>
       ))}
     </div>
