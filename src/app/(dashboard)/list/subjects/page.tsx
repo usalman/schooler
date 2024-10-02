afec401b80841a5e6a1db3344c184ecc01a6ac58
umitt <umit.salman@glomil.com>
2024-10-02 04:01:02 +0300

    feat: complete Subjects list page

--- src/app/(dashboard)/list/subjects/page.tsx
+++ src/app/(dashboard)/list/subjects/page.tsx
@@ -1,36 +1,20 @@
 import Pagination from '@/components/Pagination'
 import Table from '@/components/Table'
 import TableSearch from '@/components/TableSearch'
-import { columns } from '@/shared/TeacherListColumns'
-import { role, teachersData } from '@/shared/data'
-import { Teacher } from '@/types/Teacher'
+import { columns } from '@/shared/SubjectListColumns'
+import { subjectsData, role } from '@/shared/data'
+import { Subject } from '@/types/Subject'
 import Image from 'next/image'
 import Link from 'next/link'
 
-const TeacherListPage = () => {
-  const renderRow = (item: Teacher) => (
+const SubjectListPage = () => {
+  const renderRow = (item: Subject) => (
     <tr
       key={item.id}
       className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-PurpleLight"
     >
-      <td className="flex items-center gap-4 p-4">
-        <Image
-          src={item.photo}
-          alt=""
-          width={40}
-          height={40}
-          className="md:hidden xl:block w-10 h-10 rounded-full object-cover"
-        />
-        <div className="flex flex-col">
-          <h3 className="font-semibold">{item.name}</h3>
-          <p className="text-xs text-gray-500">{item?.email}</p>
-        </div>
-      </td>
-      <td className="hidden md:table-cell">{item.teacherId}</td>
-      <td className="hidden md:table-cell">{item.subjects.join(', ')}</td>
-      <td className="hidden md:table-cell">{item.classes.join(', ')}</td>
-      <td className="hidden md:table-cell">{item.phone}</td>
-      <td className="hidden md:table-cell">{item.address}</td>
+      <td className="flex items-center gap-4 p-4">{item.name}</td>
+      <td className="hidden md:table-cell">{item.teachers.join(', ')}</td>
       <td>
         <div className="flex items-center gap-2">
           <Link href={`/list/teachers/${item.id}`}>
@@ -52,7 +36,7 @@ const TeacherListPage = () => {
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* TOP */}
       <div className="flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold">All Teachers</h1>
+        <h1 className="hidden md:block text-lg font-semibold">All Subjects</h1>
         <div className="flex flex-col md:flex-row items-center gap-4  w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
@@ -71,11 +55,11 @@ const TeacherListPage = () => {
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={teachersData} />
+      <Table columns={columns} renderRow={renderRow} data={subjectsData} />
       {/* PAGINATION */}
       <Pagination />
     </div>
   )
 }
 
-export default TeacherListPage
+export default SubjectListPage
---
+++ src/shared/SubjectListColumns.ts
@@ -0,0 +1,14 @@
+export const columns = [
+  {
+    header: 'Subject Name',
+    accessor: 'name',
+  },
+  {
+    header: 'Teachers',
+    accessor: 'teachers',
+  },
+  {
+    header: 'Actions',
+    accessor: 'actions',
+  },
+]
