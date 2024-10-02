d0e5862944edb733eb345a9640d78ef0777e2b21
umitt <umit.salman@glomil.com>
2024-10-02 04:07:54 +0300

    feat: complete Class list page

---
+++ src/shared/ClassListColumns.ts
@@ -0,0 +1,24 @@
+export const columns = [
+  {
+    header: 'Class Name',
+    accessor: 'name',
+  },
+  {
+    header: 'Capacity',
+    accessor: 'capacity',
+    className: 'hidden md:table-cell',
+  },
+  {
+    header: 'Grade',
+    accessor: 'grade',
+    className: 'hidden md:table-cell',
+  },
+  {
+    header: 'Supervisor',
+    accessor: 'supervisor',
+  },
+  {
+    header: 'Actions',
+    accessor: 'actions',
+  },
+]
---
+++ src/types/Class.d.ts
@@ -0,0 +1,7 @@
+export type Class = {
+  id: number
+  name: string
+  capacity: number
+  grade: number
+  supervisor: string
+}
