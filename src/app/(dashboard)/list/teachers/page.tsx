34cfc8d0442dfe5dce0ec6a6cc6a21ab841bf3f8
umitt <umit.salman@glomil.com>
2024-10-02 03:20:05 +0300

    feat: create Table component for teacher list page

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -1,8 +1,50 @@
 import Pagination from '@/components/Pagination'
+import Table from '@/components/Table'
 import TableSearch from '@/components/TableSearch'
+import { columns } from '@/shared/columns'
+import { role, teachersData } from '@/shared/data'
+import { Teacher } from '@/types/Teacher'
 import Image from 'next/image'
+import Link from 'next/link'
 
-const TeacherList = () => {
+const TeacherListPage = () => {
+  const renderRow = (item: Teacher) => (
+    <tr key={item.id}>
+      <td>
+        <Image
+          src={item.photo}
+          alt=""
+          width={40}
+          height={40}
+          className="md:hidden xl:block w-10 h-10 rounded-full object-cover"
+        />
+        <div className="flex flex-col">
+          <h3 className="font-semibold">{item.name}</h3>
+          <p className="text-xs text-gray-500">{item?.email}</p>
+        </div>
+      </td>
+      <td className="hidden md:table-cell">{item.teacherId}</td>
+      <td className="hidden md:table-cell">{item.subjects.join(',')}</td>
+      <td className="hidden md:table-cell">{item.classes.join(',')}</td>
+      <td className="hidden md:table-cell">{item.phone}</td>
+      <td className="hidden md:table-cell">{item.address}</td>
+      <td>
+        <div className="flex items-center gap-2">
+          <Link href={`/list/teachers/${item.id}`}>
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-Sky">
+              <Image src="/view.png" alt="" width={16} height={16} />
+            </button>
+          </Link>
+          {role === 'admin' && (
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-Purple">
+              <Image src="/delete.png" alt="" width={16} height={16} />
+            </button>
+          )}
+        </div>
+      </td>
+    </tr>
+  )
+
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* TOP */}
@@ -24,10 +66,11 @@ const TeacherList = () => {
         </div>
       </div>
       {/* LIST */}
+      <Table columns={columns} renderRow={renderRow} data={teachersData} />
       {/* PAGINATION */}
       <Pagination />
     </div>
   )
 }
 
-export default TeacherList
+export default TeacherListPage
