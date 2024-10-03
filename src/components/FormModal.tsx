f954ca1fbdc90830f4ec5ad7bedb545feb13ac12
umitt <umit.salman.mail@gmail.com>
2024-10-03 12:03:28 +0300

    feat: make modal visible when crud cliks

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Image from 'next/image'
+import { useState } from 'react'
 
 const FormModal = ({
   table,
@@ -25,16 +26,32 @@ const FormModal = ({
   data?: any
   id?: number
 }) => {
+  const [open, setOpen] = useState<boolean>(false)
+
   const size = type === 'create' ? 'w-8 h-8' : 'w-7 h-7'
   const bgColor =
     type === 'create' ? 'bg-Yellow' : type === 'update' ? 'bg-Sky' : 'bg-Purple'
+
   return (
     <>
       <button
         className={`${size} flex items-center justify-center rounded-full ${bgColor}`}
+        onClick={() => setOpen(true)}
       >
         <Image src={`/${type}.png`} alt="" width={16} height={16} />
       </button>
+      {open && (
+        <div className="w-screen h-screen absolute left-0 top-0 bg-black bg-opacity-60 z-50 flex items-center justify-center">
+          <div className="bg-white p-4 rounded-md relative w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%]">
+            <div
+              className="absolute top-4 right-4 cursor-pointer"
+              onClick={() => setOpen(false)}
+            >
+              <Image src="/close.png" alt="" width={14} height={14} />
+            </div>
+          </div>
+        </div>
+      )}
     </>
   )
 }
