f4c77c48647171a55a0abaf985d3e73e9920405b
umitt <umit.salman.mail@gmail.com>
2025-05-08 17:45:12 +0300

    dev: get adming page UserCard data from db

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -10,10 +10,10 @@ const AdminPage = () => {
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       <div className="w-full lg:w-2/3 flex flex-col gap-8">
         <div className="flex gap-4 justify-between flex-wrap">
-          <UserCard type="student" />
+          <UserCard type="admin" />
           <UserCard type="teacher" />
+          <UserCard type="student" />
           <UserCard type="parent" />
-          <UserCard type="staff" />
         </div>
         <div className="flex gap-4 flex-col lg:flex-row">
           <div className="w-full lg:w-1/3 h-[450px]">
