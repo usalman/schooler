65068c42e11084cf4f54cfb6fb4bb3a40cdb0b9b
umitt <umit.salman.mail@gmail.com>
2025-05-15 16:21:22 +0300

    dev: make Event Calendar and Event list work according to URL param

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,11 +1,15 @@
 import Announcements from '@/components/Announcements'
-import EventCalendar from '@/components/EventCalendar'
 import FinanceChart from '@/components/FinanceChart'
 import CountChartContainer from '@/components/CountChartContainer'
 import UserCard from '@/components/UserCard'
 import AttendanceChartContainer from '@/components/AttendanceChartContainer'
+import EventCalendarContainer from '@/components/EventCalendarContainer'
 
-const AdminPage = () => {
+const AdminPage = ({
+  searchParams,
+}: {
+  searchParams: { [keys: string]: string | undefined }
+}) => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       <div className="w-full lg:w-2/3 flex flex-col gap-8">
@@ -28,7 +32,7 @@ const AdminPage = () => {
         </div>
       </div>
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
-        <EventCalendar />
+        <EventCalendarContainer searchParams={searchParams} />
         <Announcements />
       </div>
     </div>
